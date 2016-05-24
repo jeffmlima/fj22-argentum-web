ef038a5ab446084151737a0b6ab679fd6648d94e
Jefferson Mendonca Lima <jefferson.lima@tj.ac.gov.br>
2016-05-24 13:30:24 -0500

    começando com JSF

--- src/main/java/br/com/caelum/argentum/mb/ArgentumBean.java
+++ src/main/java/br/com/caelum/argentum/mb/ArgentumBean.java
@@ -1,11 +1,19 @@
 package br.com.caelum.argentum.mb;
 
+import java.io.Serializable;
 import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.argentum.modelo.Negociacao;
 import br.com.caelum.argentum.ws.ClienteWebService;
 
-public class ArgentumBean {
+@ManagedBean
+@ViewScoped
+public class ArgentumBean implements Serializable {
+	
+	private static final long serialVersionUID = -6252488051256710494L;
 	
 	private List<Negociacao> negociacoes;
 	
---
+++ src/main/java/br/com/caelum/argentum/testes/TestaWebService.java
@@ -0,0 +1,12 @@
+package br.com.caelum.argentum.testes;
+
+import br.com.caelum.argentum.ws.ClienteWebService;
+
+public class TestaWebService {
+
+	public static void main(String[] args) {
+		System.out.println(new ClienteWebService().getNegociacoes());
+
+	}
+
+}
